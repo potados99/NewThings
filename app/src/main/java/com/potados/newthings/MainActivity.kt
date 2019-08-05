91370da08b3f7c09e45699aa7195ec9d1f44f63d
potados99 <potados99@gmail.com>
2019-08-06 05:43:33 +0900

    Apply style and Navigation.

--- app/src/main/java/com/potados/newthings/MainActivity.kt
+++ app/src/main/java/com/potados/newthings/MainActivity.kt
@@ -4,14 +4,29 @@ import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        val navController = findNavController(R.id.nav_host)
 
+        val appBarConfiguration = AppBarConfiguration
+            .Builder(R.id.list_frag_dest, R.id.setting_frag_dest)
+            .build()
 
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
+
 }
---
+++ app/src/main/java/com/potados/newthings/SettingFragment.kt
@@ -0,0 +1,14 @@
+package com.potados.newthings
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.preference.PreferenceFragmentCompat
+
+class SettingFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        addPreferencesFromResource(R.xml.preference)
+    }
+}
