886eeefed58021342603b211d608c8ebaa2eb05b
potados99 <potados99@gmail.com>
2019-08-06 03:59:52 +0900

    Initial

---
+++ app/src/main/java/com/potados/newthings/ListViewModel.kt
@@ -0,0 +1,7 @@
+package com.potados.newthings
+
+import androidx.lifecycle.ViewModel
+
+class ListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/potados/newthings/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.potados.newthings
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+
+    }
+}
